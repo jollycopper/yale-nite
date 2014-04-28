873258424dcfa43dacfb90e2d13d7fda30b7d01c
Yan Wen <ian@yans-mbp.wireless.yale.internal>
2014-04-28 00:55:40 -0400

    Change profile UI style. Change Parse database.

--- night/ProfileViewController.m
+++ night/ProfileViewController.m
@@ -12,6 +12,8 @@
 
 @interface ProfileViewController ()
 
+@property (nonatomic, strong) NSMutableData *imageData;
+
 @end
 
 @implementation ProfileViewController
@@ -32,21 +34,68 @@
     if ([[PFUser currentUser] objectForKey:@"profile"][@"name"]) {
         self.nameLabel.text = [[PFUser currentUser] objectForKey:@"profile"][@"name"];
     }
-    PFImageView *profileImage = [[PFImageView alloc] init];
-    profileImage.file = [[PFUser currentUser] objectForKey:@"profileImage"];
-    [profileImage loadInBackground:^(UIImage *image, NSError *error){
-        self.roundedProfileImage.image = image;
-        [self.roundedProfileImage layoutIfNeeded];
-    }];
+    
+    if ([[PFUser currentUser] objectForKey:@"profileImage"]) {
+        PFImageView *profileImage = [[PFImageView alloc] init];
+        profileImage.file = [[PFUser currentUser] objectForKey:@"profileImage"];
+        [profileImage loadInBackground:^(UIImage *image, NSError *error){
+            self.roundedProfileImage.image = image;
+            [self.roundedProfileImage layoutIfNeeded];
+        }];
+    }
+    else {
+        [self loadImageAsyncFromFB];
+    }
     
     //add photo & friend num
     int pNum = arc4random_uniform(5);
     int fNum = arc4random_uniform(4);
-    self.numOfPhoto.text = [NSString stringWithFormat:@"%@: %d", @"Photo", pNum];
-    self.numOfFriend.text = [NSString stringWithFormat:@"%@: %d", @"Friend", fNum];
+    self.numOfPhoto.text = [NSString stringWithFormat:@"%d", pNum];
+    self.numOfFriend.text = [NSString stringWithFormat:@"%d", fNum];
     [self.numOfPhoto sizeToFit];
     [self.numOfFriend sizeToFit];
     
+}
+
+- (void) loadImageAsyncFromFB{
+    if ([[PFUser currentUser] objectForKey:@"profileImage"]) {
+        return;
+    }
+    // Download the user's facebook profile picture
+    self.imageData = [[NSMutableData alloc] init]; // the data will be loaded in here
+    
+    if ([[PFUser currentUser] objectForKey:@"profile"][@"pictureURL"]) {
+        NSURL *pictureURL = [NSURL URLWithString:[[PFUser currentUser] objectForKey:@"profile"][@"pictureURL"]];
+        
+        NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:pictureURL
+                                                                  cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                                              timeoutInterval:2.0f];
+        // Run network request asynchronously
+        NSURLConnection *urlConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self];
+        if (!urlConnection) {
+            NSLog(@"Failed to download picture");
+        }
+    }
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    // As chuncks of the image are received, we build our data file
+    [self.imageData appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    UIImage* img =[UIImage imageWithData:self.imageData];
+    // save it to parse
+    NSData *imageData = UIImagePNGRepresentation(img);
+    PFFile *imageFile = [PFFile fileWithName:@"image.png" data:imageData];
+    [imageFile saveInBackground];
+    
+    PFUser *user = [PFUser currentUser];
+    [user setObject:imageFile forKey:@"profileImage"];
+    [user saveInBackground];
+    
+    self.roundedProfileImage.image = img;
+    [self.roundedProfileImage layoutIfNeeded];
 }
 
 - (IBAction)onLogoutTouched:(id)sender {
